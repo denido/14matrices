4c4c6947ec4f73387d0c77d0a924bfd1444a3bee
uxdcy <uxdcy@student.kit.edu>
2019-05-20 22:09:49 +0200

    No mode necessary.

--- 14_matrices_main.py
+++ 14_matrices_main.py
@@ -14,14 +14,12 @@ import modules.class_fourteen_matrices
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("config",help="Config file")
-    parser.add_argument("--mode",help="run mode")
     args = parser.parse_args()
     
     with open(args.config,"r") as yamlfile:
         cfg = yaml.load(yamlfile)
         
     system = modules.class_fourteen_matrices.fourteen_matrices(cfg)
-    if args.mode == "run":
-        system.NEGF()
-        system.plot()
+    system.NEGF()
+    system.plot()
     
